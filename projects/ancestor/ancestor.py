3030710ec1e56abf88515dd7ae0e1221ca73ff66
May Ng <mngmay@gmail.com>
2020-01-21 15:03:49 -0800

    wip ancestor.py.  Have the vertices.

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,21 @@
+from graph import Graph
+
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    graph = Graph()
+    added = []
+    print(graph)
+    # make a graph inserting all of the ancestors
+    for pair in ancestors:
+        if pair[0] not in added:
+            graph.add_vertex(pair[0])
+            added.append(pair[0])
+        if pair[1] not in added:
+            graph.add_vertex(pair[1])
+            added.append(pair[1])
+        graph.add_edge(pair[0], pair[1])
+    print("vertices", graph.vertices)
+
+    for child in graph.vertices: 
+        print(child)
+    return -1
