4250d983ecb2767946287f8f9f640291cda64c8f
May Ng <mngmay@gmail.com>
2020-01-21 20:06:14 -0800

    solved ancestor. git add .! hi pinterest.

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -14,22 +14,23 @@ def earliest_ancestor(ancestors, starting_node):
             graph.add_vertex(pair[1])
             added.append(pair[1])
         graph.add_edge(pair[0], pair[1])
-    print("vertices", graph.vertices)
 
     # to find the earliest ancestor, 
     # find the parent node where the starting_node is in the set
     # repeat with the parent node until current node no longer has parents
-
-    current_earliest = None
-    node = starting_node
-    hasParents = True
     
-    while hasParents:
+    stack = Stack()
+    
+    def find_earliest(node):    
         for parent in graph.vertices: 
             if node in graph.vertices[parent]:
-                current_earliest = parent
-                node = parent
-        hasParents = False
+                stack.push(parent)
+        if stack.size() < 1:
+            if node != starting_node:
+                return node
+            else:
+                return -1
+        new_node = stack.pop()
+        return find_earliest(new_node)
     
-    print(current_earliest)
-    return current_earliest or -1
+    return find_earliest(starting_node)
