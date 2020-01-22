43a224306ebc41e913e4f68906b59371df183bd8
May Ng <mngmay@gmail.com>
2020-01-21 16:00:36 -0800

    wip - ancestor

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,10 +1,10 @@
 from graph import Graph
+from util import Stack
 
 
 def earliest_ancestor(ancestors, starting_node):
     graph = Graph()
     added = []
-    print(graph)
     # make a graph inserting all of the ancestors
     for pair in ancestors:
         if pair[0] not in added:
@@ -16,6 +16,20 @@ def earliest_ancestor(ancestors, starting_node):
         graph.add_edge(pair[0], pair[1])
     print("vertices", graph.vertices)
 
-    for child in graph.vertices: 
-        print(child)
-    return -1
+    # to find the earliest ancestor, 
+    # find the parent node where the starting_node is in the set
+    # repeat with the parent node until current node no longer has parents
+
+    current_earliest = None
+    node = starting_node
+    hasParents = True
+    
+    while hasParents:
+        for parent in graph.vertices: 
+            if node in graph.vertices[parent]:
+                current_earliest = parent
+                node = parent
+        hasParents = False
+    
+    print(current_earliest)
+    return current_earliest or -1
